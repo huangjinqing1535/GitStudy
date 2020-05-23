c221c3b09266ac79061a4a7ffee5dc3a1ba513b3
huangjinqing1535 <1120764568@qq.com>
2020-05-23 12:40:35 +0800

    第一个分支提交

--- app/src/main/java/com/huang/gitstudy/MainActivity.java
+++ app/src/main/java/com/huang/gitstudy/MainActivity.java
@@ -3,12 +3,16 @@ package com.huang.gitstudy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textView ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.tv_msg);
+        textView.setText("第一个分支");
     }
 }
